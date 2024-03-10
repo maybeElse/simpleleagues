426fd225cbd3677667ef43df4e4c2a11c82567cd
maybeElse <93097725+maybeElse@users.noreply.github.com>
2024-03-10 15:48:47 -0400

    Visual improvements

--- league/forms.py
+++ league/forms.py
@@ -4,15 +4,42 @@ from django.forms.formsets import BaseFormSet
 from django.core.exceptions import ValidationError
 
 class addPlayerForm(forms.Form):
-    playerName = forms.CharField(label="Player")
-    endPoints = forms.IntegerField(label="Points", step_size=100)
+    playerName = forms.CharField(label="Player",
+                                widget=forms.TextInput(
+                                    attrs={'class': 'form-control', 'placeholder': 'Player'}
+                                ))
+    endPoints = forms.IntegerField(label="Points", step_size=100,
+                                widget=forms.NumberInput(
+                                    attrs={'class': 'form-control', 'placeholder': 'Final score'}
+                                ))
+    penalty = forms.IntegerField(label="Penalty (only if points discarded)", step_size=1000, required=False,
+                                max_value=1000,
+                                min_value=0,
+                                widget=forms.NumberInput(
+                                    attrs={'class': 'form-control', 'placeholder': 'Penalty (if any)'}
+                                ))
+
 
 class addGameForm(forms.Form):
-    gameDate = forms.DateField(label="Date", initial=date.today)
+    gameDate = forms.DateField(label="Date", initial=date.today,
+                               widget=forms.NumberInput(
+                                   attrs={'type': 'date'}
+                               ))
     gameNote = forms.CharField(label="Notes", max_length=1000, widget=forms.Textarea(attrs={"rows":"2"}), required=False)
+    gameDiscardedRiichi = forms.IntegerField(label="Lost sticks", step_size=1000, required=False,
+                                widget = forms.NumberInput(
+                                    attrs={'placeholder': 'Riichi sticks left on table (if any)'}
+                                ))
 
 class BasePlayerFormSet(BaseFormSet):
     season = None
+
+    def __init__(self, *args, **kwargs):
+        try:
+            self.gameForm = kwargs.pop('gameForm')
+        except KeyError:
+            pass
+        super(BasePlayerFormSet, self).__init__(*args, **kwargs)
 
     def clean(self):
         if any(self.errors):
@@ -20,7 +47,10 @@ class BasePlayerFormSet(BaseFormSet):
         
         players = []
         point_tally = 0
-        duplicates = False
+        try:
+            point_tally += self.gameForm.cleaned_data['gameDiscardedRiichi']
+        except TypeError:
+            pass
 
         for form in self.forms:
             if form.cleaned_data:
@@ -30,7 +60,6 @@ class BasePlayerFormSet(BaseFormSet):
                 if player:
                     if player in players:
                         form.add_error(field="playerName", error="Duplicate player.")
-                        duplicates = True
                     players.append(player)
                 
                 point_tally += points
